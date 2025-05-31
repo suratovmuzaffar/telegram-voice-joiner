b737500b03920b7b02411542286bfa8c11e48d0b
suratovmuzaffar <suratovmuzaffar77@gmail.com>
2025-05-31 17:44:36 +0300

    fix: Railway input() problem solved

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from telethon.sync import TelegramClient
-from telethon.tl.functions.channels import InviteToChannelRequest, GetFullChannelRequest
+from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon.tl.functions.contacts import ImportContactsRequest
 from telethon.tl.types import InputPhoneContact
 
-api_id = YOUR_API_ID
-api_hash = 'YOUR_API_HASH'
+api_id = 23892150
+api_hash = 'c7582ccfdb35aaadfe4224e3d73d5d66'
 phone = '+998XXYYYYYYY'  # Telefon raqamingiz
 
 client = TelegramClient('session_name', api_id, api_hash)
@@ -12,19 +12,21 @@ client = TelegramClient('session_name', api_id, api_hash)
 async def main():
     await client.start(phone)
 
-    # Guruh username yoki link (faqat username qismi)
-    group_username = '@BRAWL_STARS_AKKAUNT_SAVDO_UZ1'  # Masalan: 'mygroup'
-
-    # Guruhni olish
+    # Guruh username (masalan: 'uzbek_chat') - faqat @ dan keyingi qismi
+    group_username = 'BRAWL_STARS_CHAT'
     group = await client.get_entity(group_username)
 
-    # Userni qo‘shish (raqam orqali kontakt sifatida)
-    contact = InputPhoneContact(client_id=0, phone='+998ZZZZZZZZZ', first_name='Name', last_name='')
-
+    # Foydalanuvchini telefon raqami orqali kontakt sifatida qo‘shish
+    contact = InputPhoneContact(
+        client_id=0,
+        phone='+998ZZZZZZZZZ',  # Bu foydalanuvchining raqami
+        first_name='Name',
+        last_name=''
+    )
     result = await client(ImportContactsRequest([contact]))
     user = result.users[0]
 
-    # Kanalga (yoki megagroup) qo‘shish
+    # Kanal yoki megagroupga qo‘shish
     await client(InviteToChannelRequest(
         channel=group,
         users=[user]
