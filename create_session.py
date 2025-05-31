a67819377552ae91634148b44b8cd8319903d13a
suratovmuzaffar <suratovmuzaffar77@gmail.com>
2025-05-31 18:59:34 +0300

    yahshilangan

---
+++ create_session.py
@@ -0,0 +1,10 @@
+from telethon.sync import TelegramClient
+
+api_id = 23892150
+api_hash = 'c7582ccfdb35aaadfe4224e3d73d5d66'
+phone = '+998333439909'  # O'z telefon raqamingizni shu yerga yozing
+
+client = TelegramClient('session_name', api_id, api_hash)
+
+client.start(phone)
+print("✅ Session fayli yaratildi!")
